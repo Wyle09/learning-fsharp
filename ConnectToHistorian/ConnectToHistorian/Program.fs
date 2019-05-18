87c12f86536aedde5432a66d95e98b99dc365069
root <root@LAPTOP-F9RBL54D.localdomain>
2019-05-18 18:15:59 -0400

    First commit

---
+++ ConnectToHistorian/ConnectToHistorian/Program.fs
@@ -0,0 +1,74 @@
+﻿open System.Data.SqlClient
+open System
+open FSharp.Data
+open System.Collections.Generic
+
+[<Literal>]
+let _query = 
+    @"--Quering the WideHistory table in INSQL liked server
+        DECLARE @tagNames NVARCHAR(MAX) = '',
+	        @openQuery NVARCHAR(MAX),
+	        @tsql NVARCHAR(MAX),
+	        @linkedServer NVARCHAR(MAX) = 'INSQL';
+
+            SELECT @tagNames += QUOTENAME(TagName, '[') + ','
+            FROM TagRef
+            WHERE TagName LIKE '%Mixer%';
+
+            SET @tagNames = LEFT(@tagNames, LEN(@tagNames) - 1)
+            SET @tsql = 'SELECT DateTime, ' + @tagNames + ' 
+                        FROM WideHistory
+                        WHERE DateTime > DATEADD(HH, -4, GETDATE())';
+            SET @openQuery = 'SELECT * 
+                            FROM OPENQUERY(' + @linkedServer + ','' ' + @tsql + ' '' ) 
+                            ORDER BY DateTime ASC';
+
+            EXECUTE(@openQuery);"
+
+let _conn = new SqlConnection(@"Data Source=WIN-UIH9QHSEMDC;Initial Catalog=Runtime;Integrated Security=false;User Id=sa;Password=Password_1")
+
+let queryToList() : List<string> =    
+    let _lst = new List<string>()
+    let _cmd = new SqlCommand(_query, _conn)
+    
+    try 
+        try 
+            _conn.Open()
+            let _reader : SqlDataReader = _cmd.ExecuteReader()
+            let _headers = 
+                for _i in 0.. _reader.FieldCount do                     
+                    _lst.Add(String.Concat(",", _reader.GetName.ToString()))
+
+            let _lines = 
+                while _reader.Read() do 
+                    _lst.Add(_reader.GetValue(1).ToString())
+
+            _conn.Close()
+            _conn.Dispose()
+            _lst
+        
+        with
+            | :? SqlException as ex -> printf "Connection Failiure: %s" <| ex.Message |> Console.ReadLine |> ignore; _lst
+
+    finally
+        _conn.Close()
+        _conn.Dispose()
+        _lst |> ignore
+        
+    
+let dataToCSV(_lst : List<string>) = 
+    if _lst <> null then          
+        let _valueLines = [for _row in _lst -> String.concat ",", _row.ToString]
+        _valueLines 
+        |> Seq.iter(fun _x -> printfn "%s" <| _x.ToString |> Console.ReadLine |> ignore
+
+
+        //let _sw = new System.IO.StreamWriter(_filePath, true)
+        //_valueLines |> List.map(string) |> _sw.Write
+
+[<EntryPoint>]
+let main argv = 
+    let _filePath = String.Format("{0}{1}{2}{3}", "C:\\Users\\wyle.cordero\\Desktop\\", "Test", DateTime.Now.ToString("yyyyMMddHHmmssfff"), ".csv")
+    let _lst = queryToList()
+    let _data = dataToCSV(_lst)
+    0
---
+++ Udemy FSharp Cource/DataPrograming/DataPrograming/LoanPayments.fs
@@ -0,0 +1,86 @@
+﻿module DataProgramin
+
+open System.IO
+open System
+open System.Collections.ObjectModel
+
+let lines = 
+    File.ReadAllLines(@"C:\My Projects\FSharp\Data\Loan payments data.csv")
+    |> Array.distinct
+    |> Array.map(fun s -> s.Split(','))
+
+let header = lines |> Array.take 1 // Get headers.
+let data = lines |> Array.skip 1 // Get all data except headers.
+
+[<Measure>] type days
+
+type LoanStatus = 
+    | PaidOff of PaidOffTime : DateTime
+    | Collection of PastDueDays : int<days>
+    | Collection_PaidOff of PaidOffTime : DateTime * PastDueDays : int<days>
+
+[<Measure>] type dollar
+
+//let transformtoloanstatus status = 
+//    match status with
+//    | "paidoff" -> PaidOff
+//    | "collection" -> Collection
+//    | "collection_paidoff" -> Collection_PaidOff
+//    | unknown -> failwith(sprintf "unrecongnized loan status: \"%s\"" unknown)
+
+let transformToLoanStatus(status, paidOffTime, pastDueDays) = 
+    match status with
+    | "PAIDOFF" 
+        -> PaidOff(DateTime.Parse(paidOffTime))
+    | "COLLECTION" 
+        -> Collection(Int32.Parse(pastDueDays) * 1<days>)
+    | "COLLECTION_PAIDOFF" 
+        -> Collection_PaidOff(DateTime.Parse(paidOffTime), (Int32.Parse(pastDueDays)) * 1<days>)
+    | unknown 
+        -> failwith(sprintf "unrecongnized loan status: \"%s\"" unknown)
+
+type LoanPayment = 
+    {
+        LoanId : string;
+        LoanStatus : LoanStatus;
+        Principal : int<dollar>;
+        Terms : string;
+        EffectiveDate : string;
+        DueDate : string;
+        PaidOffTime : string;
+        PastDueDate : string;
+        Age : string;
+        Education : string;
+        Gender : string;
+    }
+
+let transformToLoanPaymentData(data : string array) = // function example.
+    {
+        LoanId = data.[0];
+        LoanStatus = transformToLoanStatus(data.[1], data.[6], data.[7]);
+        Principal = Int32.Parse(data.[2]) * 1<dollar>;
+        Terms = data.[3];
+        EffectiveDate = data.[4];
+        DueDate = data.[5];
+        PaidOffTime = data.[6];
+        PastDueDate = data.[7];
+        Age = data.[8];
+        Education = data.[9];
+        Gender = data.[10];
+    }
+
+let paymentData = // Call the "tranformToLoanPaymentData" function.
+    data
+    |> Array.map transformToLoanPaymentData
+
+// Example below, will find the distinct values for "loanStatus", can do the same for the 
+// other columns as well.
+//let distinctLoanStatusValues = 
+//    paymentData
+//    |> Array.map(fun pd -> pd.LoanStatus)
+//    |> Array.distinct
+
+//let distinctPrincipalValues = 
+//    paymentData
+//    |> Array.map(fun pd -> pd.Principal)
+//    |> Array.distinct
